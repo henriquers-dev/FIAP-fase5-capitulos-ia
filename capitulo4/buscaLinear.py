0205d8817b01516ab589ac663518ef329bc7784f
Henrique Siqueira <henrique.ribeiro1201@gmail.com>
2026-09-01 14:03:47 -0300

    Add linear search function in buscaLinear.py
    Implement linear search function to find index of a number.

--- capitulo4/buscaLinear.py
+++ capitulo4/buscaLinear.py
@@ -1,1 +1,7 @@
+def linear_search(numbers: list[int], k: int) -> int:
+    for idx, num in enumerate(numbers):
+        if num == k:
+            return idx
+    return -1
 
+print(linear_search([1, 4, 2, 5, 3, 4, 8], 3))  # Saída: 4
